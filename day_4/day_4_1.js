9c33591c769c40dc53bd6a913066dcfaf4034c63
Jesse M. Porch <porchjm@gmail.com>
2023-12-04 09:21:36 -0500

    feat: day 4_1

---
+++ day_4/day_4_1.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+fs.readFile('./day_4.dat', 'utf8', (err, data) => {
+  let lines = data.split('\r\n');
+  const total = processLines(lines);
+
+  console.log(`Answer: ${total}`);
+});
+
+function processLines(lines) {
+  let total = 0;
+  for (let line of lines) {
+    let val = 0;
+    const winnersSet = new Set();
+    [winners, picks] =  line.replaceAll('  ',' ').split(': ')[1].split(' | ');
+    for (winner of winners.split(' ')) {
+      winnersSet.add(winner);
+    }
+    picks = picks.split(' ');
+    for (pick of picks) {
+      if (winnersSet.has(pick)) {
+        val = val > 0 ? val * 2 : 1;
+      }
+    }
+    total += val;
+  }
+  return total;
+}
